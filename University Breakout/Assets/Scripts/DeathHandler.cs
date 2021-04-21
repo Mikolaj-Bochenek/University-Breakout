c851585ca94e223280a7d30e3808f5c2caa9f021
Mikolaj Bochenek <mikolajbochenek.dev@gmail.com>
2021-04-21 21:03:43 +0200

    Add input to select weapon

--- University Breakout/Assets/Scripts/DeathHandler.cs
+++ University Breakout/Assets/Scripts/DeathHandler.cs
@@ -4,15 +4,13 @@ public class DeathHandler : MonoBehaviour
 {
     [SerializeField] Canvas gameOverCanvas;
 
-    void Start()
-    {
-        gameOverCanvas.enabled = false;
-    }
+    void Start() => gameOverCanvas.enabled = false;
 
     public void HandleDeath()
     {
         gameOverCanvas.enabled = true;
         Time.timeScale = 0;
+        FindObjectOfType<WeaponSwitcher>().enabled = false;
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
     }
