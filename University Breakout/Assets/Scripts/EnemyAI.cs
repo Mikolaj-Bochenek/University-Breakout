c8f668ef4daa13c58f88b25948fb681180df77ab
Mikolaj Bochenek <mikolajbochenek.dev@gmail.com>
2021-04-21 10:02:33 +0200

    Create player health class

--- University Breakout/Assets/Scripts/EnemyAI.cs
+++ University Breakout/Assets/Scripts/EnemyAI.cs
@@ -40,7 +40,6 @@ public class EnemyAI : MonoBehaviour
     void AttackTarget()
     {
         GetComponent<Animator>().SetBool("attack", true);
-        Debug.Log($"{name} has seeked and is destroying {target.name}");
     }
     void OnDrawGizmosSelected()
     {
