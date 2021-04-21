3f4769e8ba356e21d6bc8a0e47bee230d10050ab
Mikolaj Bochenek <mikolajbochenek.dev@gmail.com>
2021-04-21 13:43:33 +0200

    Add weapon zoom mouse sensitivity

--- University Breakout/Assets/Scripts/WeaponZoom.cs
+++ University Breakout/Assets/Scripts/WeaponZoom.cs
@@ -1,12 +1,22 @@
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] Camera fpsCamera;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomOutSensitivity = 2f;
+    [SerializeField] float zoomInSensitivity = .5f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomedInToggle = false;
+
+    void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     void Update()
     {
@@ -14,6 +24,8 @@ public class WeaponZoom : MonoBehaviour
         {
             zoomedInToggle = !zoomedInToggle;
             fpsCamera.fieldOfView = zoomedInToggle ? zoomedInFOV : zoomedOutFOV;
+            fpsController.mouseLook.XSensitivity = zoomedInToggle ? zoomInSensitivity : zoomOutSensitivity;
+            fpsController.mouseLook.YSensitivity = zoomedInToggle ? zoomInSensitivity : zoomOutSensitivity;
         }
     }
 }
