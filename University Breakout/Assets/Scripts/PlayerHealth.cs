8517aa462ccb062222f081c99da3943a647bc648
Mikolaj Bochenek <mikolajbochenek.dev@gmail.com>
2021-04-24 10:04:54 +0200

    Add menu

--- University Breakout/Assets/Scripts/PlayerHealth.cs
+++ University Breakout/Assets/Scripts/PlayerHealth.cs
@@ -4,9 +4,19 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
 
+    HealthBar healthBar;
+
+    void Start()
+    {
+        healthBar = GetComponent<HealthBar>();
+        healthBar.SetMaxHealth(hitPoints);
+    }
+
     public void TakeDamage(float damage)
     {
         hitPoints -= damage;
+        healthBar.SetHealth(hitPoints);
+
         if (hitPoints <= 0)
         {
             GetComponent<DeathHandler>().HandleDeath();
